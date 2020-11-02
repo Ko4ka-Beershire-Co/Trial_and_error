9c1b7c18a80bbb7326bf3d09645b454638224947
Alexander Kuznetsov <alex@beershire.ru>
2020-11-02 14:30:29 +0300

    Update Normal_dist.py

--- Normal_dist.py
+++ Normal_dist.py
@@ -3,19 +3,24 @@
 
 import numpy as np
 import plotly.express as plt
+import scipy.stats as stats
+
 
 mu, sigma, n = 50, 20, 1000
-
+lower, upper = 0, 100
 
 def normal(x, mu, sigma):
     return (2. * np.pi * sigma ** 2.) ** -.5 * np.exp(-.5 * (x - mu) ** 2. / sigma ** 2.)
 
+X = stats.truncnorm.rvs(
+    (lower - mu) / sigma, (upper - mu) / sigma, loc=mu, scale=sigma, size=n)
 
 x = np.random.normal(mu, sigma, n)
-y = normal(x, mu, sigma)
+y = normal(X, mu, sigma)
 
-# print(x)
-# print(y)
+print(X)
+print(x)
+print(y)
 
-fig = plt.scatter(x=x, y=y)
+fig = plt.scatter(x=X, y=y)
 fig.show()
