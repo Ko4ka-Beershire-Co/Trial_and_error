6e82a686921638890de4706773e40fd81eb0edaf
Alexander Kuznetsov <alex@beershire.ru>
2021-02-20 11:58:25 +0300

    Create Rabota.js

---
+++ Rabota.js
@@ -0,0 +1,59 @@
+# About-Us {
+	Company.website = "http://activebc.ru"
+	Company.size = "1500 ppl"
+	Company.name = "ActiveBC"
+	Company.motto = "Пишите код так, как будто поддерживать его будет склонный к насилию психопат, который знает, где вы живёте. Джон Ф. Вудс"
+	# Company.description = "Компания «АБК» является одним из лидеров в области речевых технологий, автоматизации деятельности коллцентров и взаимодействия с клиентами. Компания разрабатывает и поддерживает различные проекты и торговые площадки в финансовом секторе."
+}
+
+# Мы-ищем{
+	.hard-skills{
+		@import JS; vue.js; CSS; HTML;
+		@import bootstrap;
+	}
+	.soft-skills{
+		@import "head/brain";"perfectionism";"reliability";"humor";
+		@import "work-from-home-ready";"team-player";"experience >= 3 years";
+		@import "I-love-my-job";"I-love-to-code";
+	}
+}
+
+# Всему остальному научим, нужно только желание!
+
+# What-МЫ-Предлагаем? {
+	.zero(Us) {
+		Money: Конкурентная; White;
+		Company:   Solid; Big; Stable;
+		Future:   Career-рост; Personal-рост;
+		Bonuses:   ДМС; Годовые-bonuses;
+}
+	.first(Team) {
+		End-to-end:   front; back; designers; managers; testers; DevOps;
+		Properties:   friendly; cool; fast;
+}
+	.second(Work) {
+		Format:   Гибкий-time; Work-from-home;
+		Agile: "sprint-2-weeks"; planning; development; Gitlab + CI/CD; code-review;
+		Sprint:   Planning; Development; Demo; Sprint-review
+		Git: Gitlab + CI/CD;
+}
+	.third(Projects) {
+		Existing:   Marketplace; Website; App;
+		New:   Coming-soon;
+	}
+}
+
+# ContactInfo {
+	if (hard-skills && soft-skills && interested && ready) {
+		contact.e-mail = 'hr@activebc.ru'
+		contact.number = '8.800.555.35.35'
+		contact.whatsapp = '8.800.555.35.35'
+		contact.telegram = '@123'
+	}
+	else {
+		'Have a nice day!'
+	}
+
+}
+
+# Код писал HR, не судите строго:)
