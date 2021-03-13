404b767d53ff55c6628bbb9935b339448593dd63
Alexander Kuznetsov <alex@beershire.ru>
2021-03-13 18:33:23 +0300

    Create Job.js

---
+++ Job.js
@@ -0,0 +1,66 @@
+/* Закос под JS */
+
+class Our_company {
+
+	constructor(website, size, name, motto, description) {
+		this.website = 'http://activebc.ru';
+		this.size = 1500; //сотрудников
+		this.name = 'ActiveBC';
+		this.motto = 'Пишите код так, как будто поддерживать его будет склонный к насилию психопат, который знает, где вы живёте. Джон Ф. Вудс';
+		this.description = 'Компания «АБК» является одним из лидеров в области речевых технологий, автоматизации деятельности коллцентров и взаимодействия с клиентами.'
+		'Компания разрабатывает и поддерживает различные проекты и торговые площадки в финансовом секторе.';
+	}
+}
+
+// Мы ищем того, кто умеет:
+import JS from 'Skills';
+import CSS from 'Skills';
+import HTML from 'Skills';
+import Vue from 'Skills';
+import bootstrap from 'Skills';
+// ... и вот так
+import brain from 'Head';
+import perfectionism from 'Features';
+import reliability from 'Features';
+import experience from 'Life';
+import humor from 'Features';
+// Всему остальному научим, нужно только желание! */
+
+#We-Offer {
+	.us {
+		Money: 'Конкурентная', 'White';
+		Company: 'Solid', 'Big', 'Stable';
+		Future: 'Career-рост', 'Personal-рост';
+		Bonuses: 'ДМС', 'Годовые-bonuses';
+	}
+	.team {
+		End-to-end: 'front', 'back', 'designers', 'managers', 'testers', 'DevOps';
+		Properties: 'friendly', 'cool', 'fast';
+	}
+	.work {
+		Format: 'Гибкий-time', 'Work-from-home';
+		Agile: 'sprint-2-weeks', 'planning; development', 'Gitlab + CI/CD', 'code-review';
+		Sprint: 'Planning', 'Development', 'Demo', 'Sprint-review';
+		Git: 'Gitlab + CI/CD';
+	}
+	.projects {
+		Existing: 'Marketplace', 'Website', 'App';
+		New: 'Coming-soon';
+	}
+}
+
+/* Тут уже JS:) */
+
+function contacts(hard_skills, soft_skills, interested, ready) {
+
+	if (hard_skills && soft_skills && interested && ready) {
+		contact_email('hr@activebc.ru');
+		contact_number('8.800.555.35.35');
+		contact_whatsapp('8.800.555.35.35');
+		contact_telegram('@123');
+
+	} else {
+
+		'Have a nice day!'
+	}
+}
